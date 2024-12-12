510521c8a9f0d5a60f096d19246d0f282bdb61f6
eftakhar-491 <eftt042@gmail.com>
2024-12-12 16:38:15 +0600

    job details section done

--- src/components/Pages/JobDetails.jsx
+++ src/components/Pages/JobDetails.jsx
@@ -1,5 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function JobDetails() {
-  return <div>JobDetails</div>;
+  const { id } = useParams();
+  console.log(id);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["jobs", id],
+    queryFn: () => axios.get(`http://localhost:5000/jobs/${id}`),
+  });
+  console.log(data);
+  if (isLoading) {
+    return <h1>loading...</h1>;
+  }
+
+  return (
+    <>
+      <section className="rounded-lg text-white bg-slate-800/20 backdrop-blur-sm">
+        <h1 className="text-2xl font-bold pl-4 pt-4 ">Job Details</h1>
+        <p className=" pl-4 text-[12px]">
+          Make sure your skils are matched either please don't apply !
+        </p>
+        <div className="p-4 ">
+          <div className="flex items-center gap-2 mb-3">
+            <img className="w-14 h-14" src={data.data.company_logo} alt="" />
+            <div>
+              <h1 className="text-lg font-bold">{data.data.company}</h1>
+              <p className="text-sm font-medium">{data.data.location}</p>
+            </div>
+          </div>
+          <p className="text-2xl font-bold">{data.data.title}</p>
+          <h2 className="text-lg font-semibold mt-3">Description:</h2>
+          <p className="text-sm mb-3">{data.data.description}</p>
+          <h2 className="text-lg font-semibold">Requirements:</h2>
+          <ul className="list-disc ml-4">
+            {data.data.requirements.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+          <h2 className="text-lg font-semibold mt-3">Responsibilities:</h2>
+          <ul className="list-disc pl-4">
+            {data.data.responsibilities.map((item) => (
+              <li>{item}</li>
+            ))}
+          </ul>
+          <p className="font-semibold mb-3 mt-3">
+            Job Type: {data.data.jobType}
+          </p>
+          <h2 className="text-lg font-semibold mb-2">
+            Salary Range: {data.data.salaryRange.min} -{" "}
+            {data.data.salaryRange.max} {data.data.salaryRange.currency}{" "}
+          </h2>
+          <hr />
+          <h3 className="mt-3">HR Name : {data.data.hr_name}</h3>
+          <h4>HR Email : {data.data.hr_email}</h4>
+          <h5>Last date of Application: {data.data.applicationDeadline}</h5>
+          <button className="px-4 py-1 border rounded mt-4">Apply Now</button>
+        </div>
+      </section>
+    </>
+  );
 }
